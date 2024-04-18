ab3ae5d07a3cea626bb051a154db047a47fc47fb
Julia Kazarina <yastrebovayv@gmail.com>
2024-04-18 17:56:17 +0500

    changed models for tests

---
+++ dodo/Models/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  dodo
+//
+//  Created by Юлия Ястребова on 18.04.2024.
+//
+
+import Foundation
+
+struct Category: Codable {
+    let id: UInt
+    let name: String
+    let indexPath: [Int]
+}
