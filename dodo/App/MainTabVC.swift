8e5280b971f47187158152b6d47e45911dd82c25
Julia Kazarina <yastrebovayv@gmail.com>
2024-10-22 11:07:00 +0500

    don't use tabbar controller

--- dodo/App/MainTabVC.swift
+++ dodo/App/MainTabVC.swift
@@ -7,48 +7,48 @@
 
 //MARK: - НЕ используется
 
-import UIKit
-
-class MainTabVC:UITabBarController {
-    
-    private var menuController: MenuVC = {
-        let controller = MenuConfigurator().configure()
-        let tableItem = UITabBarItem(title: "Меню", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house"))
-        controller.tabBarItem = tableItem
-        return controller
-    }()
-    
-    private var mapController: MapVC = {
-        let controller = MapVC()
-        let tableItem = UITabBarItem(title: "", image: UIImage(systemName: "location"), selectedImage: UIImage(systemName: "location"))
-        controller.tabBarItem = tableItem
-        return controller
-    }()
-    
-    private var cartController: CartVC = {
-        let controller = CartConfigurator().configure()
-        let tableItem = UITabBarItem(title: "Корзина", image: UIImage(systemName: "cart"), selectedImage: UIImage(systemName: "cart"))
-        controller.tabBarItem = tableItem
-        return controller
-    }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setUpTabBar()
-    }
-}
-
-extension MainTabVC {
-    private func setUpTabBar() {
-        let appearance = UITabBarAppearance()
-        appearance.stackedLayoutAppearance.normal.iconColor = UIColor.gray
-        appearance.stackedLayoutAppearance.selected.iconColor = UIColor.black
-        
-        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.gray]
-        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
-        
-        tabBar.standardAppearance = appearance
-        tabBar.scrollEdgeAppearance = appearance
-        viewControllers = [menuController, mapController, cartController]
-    }
-}
+//import UIKit
+//
+//class MainTabVC:UITabBarController {
+//    
+//    private var menuController: MenuVC = {
+//        let controller = MenuConfigurator().configure()
+//        let tableItem = UITabBarItem(title: "Меню", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house"))
+//        controller.tabBarItem = tableItem
+//        return controller
+//    }()
+//    
+//    private var mapController: MapVC = {
+//        let controller = MapVC()
+//        let tableItem = UITabBarItem(title: "", image: UIImage(systemName: "location"), selectedImage: UIImage(systemName: "location"))
+//        controller.tabBarItem = tableItem
+//        return controller
+//    }()
+//    
+//    private var cartController: CartVC = {
+//        let controller = CartConfigurator().configure()
+//        let tableItem = UITabBarItem(title: "Корзина", image: UIImage(systemName: "cart"), selectedImage: UIImage(systemName: "cart"))
+//        controller.tabBarItem = tableItem
+//        return controller
+//    }()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        setUpTabBar()
+//    }
+//}
+//
+//extension MainTabVC {
+//    private func setUpTabBar() {
+//        let appearance = UITabBarAppearance()
+//        appearance.stackedLayoutAppearance.normal.iconColor = UIColor.gray
+//        appearance.stackedLayoutAppearance.selected.iconColor = UIColor.black
+//        
+//        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.gray]
+//        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+//        
+//        tabBar.standardAppearance = appearance
+//        tabBar.scrollEdgeAppearance = appearance
+//        viewControllers = [menuController, mapController, cartController]
+//    }
+//}
